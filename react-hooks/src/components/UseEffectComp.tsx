00acb8f0f6beeca1aeda1a4e916a18e770ccfffc
priopambudi <prio@vcgamers.com>
2024-03-16 23:34:05 +0700

    use effect and use memo

---
+++ react-hooks/src/components/UseEffectComp.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+function UseEffectComp() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // The code that we want to run
+    console.log("The count is:", count);
+
+    // Optional return function
+    return () => {
+      console.log("I am being cleaned up!");
+    };
+  }, [count]); // The dependency array
+
+  return (
+    <div className="tutorial container mx-auto">
+      <h1>Count: {count}</h1>
+      <button
+        className="bg-red-500 p-2 rounded-md text-white mr-2"
+        onClick={() => setCount(count - 1)}
+      >
+        Decrement
+      </button>
+      <button
+        className="bg-blue-500 p-2 rounded-md text-white"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default UseEffectComp;
---
+++ react-hooks/src/components/UseMemoComp.tsx
@@ -0,0 +1,32 @@
+import { useMemo, useState } from "react";
+import { initialItems } from "../utils/memo";
+
+function UseMemoComp() {
+  const [count, setCount] = useState(0);
+  const [items] = useState(initialItems);
+
+  // everytime we click the button, it will rerender the component. and the selectedItem will rerender too
+  // It make a performance issue
+  // with useMemo, it will keep the value and will not rerendered the selectedItem
+  // until the value in dependencies changed, the memo will triggered
+  // it will reduce the preformance
+  const selectedItem = useMemo(
+    () => items.find((item) => item.isSelected),
+    [items]
+  );
+
+  return (
+    <div className="tutorial container mx-auto">
+      <h1>Count: {count}</h1>
+      <h1>Selected Item:{selectedItem?.id}</h1>
+      <button
+        className="bg-blue-500 p-2 rounded-md text-white font-semibold"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default UseMemoComp;
